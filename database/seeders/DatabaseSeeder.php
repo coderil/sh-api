25b1abaa54c894449dbc5bd65f15ade0e1bbeaea
coderil <reillefrane@gmail.com>
2026-01-21 10:52:16 +0800

    Set up spatie, add roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,12 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'haerin',
+            'email' => 'haerin@example.com'
+        ]);
+
+        $this->call([
+            RoleSeeder::class
         ]);
     }
 }
