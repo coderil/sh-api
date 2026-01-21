1a5edd460310c58cd2d26758614d48272830e757
coderil <reillefrane@gmail.com>
2026-01-21 10:52:45 +0800

    Add data to user table

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,6 +28,7 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'phone_number' => '09123456789',
             'remember_token' => Str::random(10),
         ];
     }
