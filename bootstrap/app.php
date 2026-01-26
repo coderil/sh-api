7e7b016c2e63092dc90f3c9e9702ecd17845af82
coderil <reillefrane@gmail.com>
2026-01-26 16:09:32 +0800

    feat: add handler for unauthenticated user, and undefined  route

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +17,14 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->renderable(function(RouteNotFoundException $e) {
+            return response()->json([
+                "message" => 'Unauthenticated'
+            ]);
+        }); 
+        $exceptions->renderable(function(NotFoundHttpException $e) {
+            return response()->json([
+                "message" => 'Route not found'
+            ]);
+        }); 
     })->create();
