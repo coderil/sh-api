7aea5bf56526bf759f3975585fa52a261176e470
coderil <reillefrane@gmail.com>
2026-01-26 16:10:06 +0800

    feat: Add routes for shop and products

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Product\ProductController;
+use App\Http\Controllers\Shop\ShopController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -17,4 +20,27 @@ Route::group([
     $route->post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 });
 
+// Route::get('/email/verify/{id}/{hash}', [AuthController::class, 'verifyEmail'])->middleware(['auth:sanctum', 'signed'])->name('verification.verify');
+
+Route::group([
+    'prefix' => 'shops',
+    'middleware' => 'auth:sanctum'
+], function($route) {
+    $route->get('/', [ShopController::class, 'index']);
+    $route->post('/', [ShopController::class, 'store']);
+    $route->get('/{shop}', [ShopController::class, 'show']);
+    $route->put('/{shop}', [ShopController::class, 'update']);
+    $route->delete('/{shop}', [ShopController::class, 'destroy']);
+});
+
+Route::group([
+    'prefix' => 'products'
+], function($route) {
+    $route->get('/', [ProductController::class, 'index']);
+    $route->get('/{id}', [ProductController::class, 'show']);
+
+    $route->post('/', [ProductController::class, 'store']);
+    $route->post('/{id}', [ProductController::class, 'update']);
+    $route->post('/{id}', [ProductController::class, 'destroy']);
+});
 
