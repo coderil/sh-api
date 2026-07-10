b9c74c2ace44f2948a225d7b06b076cc108c20f4
franlyy <reillefrane@gmail.com>
2026-07-10 09:49:43 +0800

    feat: Implement automatic documentation using scramble

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,14 @@
 
 namespace App\Providers;
 
+use Dedoc\Scramble\Scramble;
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\SecurityRequirement;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
 use Illuminate\Support\ServiceProvider;
+use Dedoc\Scramble\Support\Generator\Operation;
+use Dedoc\Scramble\Support\RouteInfo;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +26,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Scramble::configure()
+            ->withDocumentTransformers(function (OpenApi $openApi) {
+                $openApi->components->securitySchemes['sanctum'] =
+                    SecurityScheme::http('bearer');
+            })
+            ->withOperationTransformers(function (
+                Operation $operation,
+                RouteInfo $routeInfo
+            ) {
+                $middleware = $routeInfo->route->gatherMiddleware();
+
+                if (in_array('auth:sanctum', $middleware, true)) {
+                    $operation->security = [
+                        new SecurityRequirement([
+                            'sanctum' => [],
+                        ]),
+                    ];
+                }
+        });
     }
 }
